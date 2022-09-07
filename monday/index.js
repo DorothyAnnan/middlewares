3bf74c717b99073805a42733a35cb012d9d976da
Dorothy Annan <dorothyannan01@gmail.com>
2022-09-07 14:57:02 -0700

    initial commit

---
+++ monday/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const userRouter = require("./routes/users.route");
+const { globalMiddleware } = require("./middlewares/globalMiddleware");
+
+const app = express();
+
+app.use(globalMiddleware);
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("Welcome to my server. Please use /users to get all users");
+});
+app.use("/users", userRouter);
+
+app.listen(4000, () => {
+  console.log("Server listening on http://localhost:4000/");
+});
---
+++ monday/routes/users.route.js
@@ -0,0 +1,16 @@
+const router = require("express").Router;
+const {
+  getUser,
+  getUsers,
+  updateUser,
+  createUser,
+  deleteUser,
+} = require("../controllers/users.controller");
+const { pathMiddleware } = require("../middlewares/pathMiddleware");
+
+const userRouter = router();
+
+userRouter.route("/").get(pathMiddleware, getUsers).post(createUser);
+userRouter.route("/:userId").get(getUser).patch(updateUser).delete(deleteUser);
+
+module.exports = userRouter;
